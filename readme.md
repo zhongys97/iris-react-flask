7ae68c28d70e9b77ec684dc29c241eadedeb5bf8
Yishan Zhong <yzhong307@gatech.edu>
2022-05-20 03:20:52 -0400

    full stack

---
+++ readme.md
@@ -0,0 +1,6 @@
+# deploy on AWS
+
+`sudo chown -R $(whoami) /usr/local/bin`
+
+` curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose`
+`sudo chmod +x /usr/local/bin/docker-compose`
--- server/app.py
+++ server/app.py
@@ -23,7 +23,7 @@ def hello():
         request_data = request.data
         request_data = json.loads(request_data.decode('utf-8'))
         # print("\n")
-        # print(request_data)
+        print(request_data)
         species_name = get_species(request_data)
         # print(species_name)
         return {"201": "success todo"}
